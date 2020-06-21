91dd4882f7c136d8dca1e9d03c6b82648ed3681f
Adam Slaski <aslaski@atlassian.com>
2020-06-21 19:19:33 +0200

    angular frontend

--- phrase-frontend/src/app/app.component.ts
+++ phrase-frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'phrase-frontend';
+  public data = "no data";
+
+  constructor(private http: HttpClient) { }
+
+  get() {
+    this.http.get("/spring-boot-http-service/", { observe: 'response', responseType: 'text' }).subscribe((results: HttpResponse<string>) => {
+      this.data = results.body;
+    })
+  }
+
+  ngOnInit() {
+    this.get();
+  }
+
 }
